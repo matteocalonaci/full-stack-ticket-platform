ee7f9973637c73139b6525574a59f20ce7d9f1ed
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-11-20 13:54:41 +0100

    importo immagine e cambio nome browser

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,11 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Ticket-Platform') }}</title>
+
+      <!-- Favicon -->
+    <link rel="icon" href="{{ asset('images/icon-ticket-platform.png') }}" type="image/x-icon">
+
 
     <!-- Fontawesome 6 cdn -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
